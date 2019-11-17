1609b4b13139804954acf7ff217dfe56a57aba0b
alizza-ops <52742283+alizza-ops@users.noreply.github.com>
2019-11-17 10:10:09 +0700

    Add files via upload

---
+++ tugas24.js
@@ -0,0 +1,12 @@
+const pengurutanArray = () => {
+var data =[32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+console.log("Sebelumnya : "+data)
+data = data.sort();
+console.log("Ascending : "+data);
+data = data.reverse()
+console.log("Descending : "+data);
+
+
+}
+
+pengurutanArray();
